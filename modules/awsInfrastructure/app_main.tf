5f16e7ed8efd17669a6cb9f1a2db224a41b9ad79
Heath Parrott <heath.parrott@gmail.com>
2020-04-22 09:44:06 -0700

    updates VPCE SG to be filtered to VPC CIDRs
    Changed all VPCEs in all VPCs

--- modules/awsInfrastructure/app_main.tf
+++ modules/awsInfrastructure/app_main.tf
@@ -206,13 +206,13 @@ resource "aws_security_group" "sg_internal_application_vpc" {
 		protocol = -1
 		from_port = 0
 		to_port = 0
-		cidr_blocks = ["0.0.0.0/0"]
+		cidr_blocks = ["${var.cidr-2}"]
 	}
 	egress {
 		protocol = -1
 		from_port = 0
 		to_port = 0
-		cidr_blocks = ["0.0.0.0/0"]
+		cidr_blocks = ["${var.cidr-2}"]
 	}
 	vpc_id = aws_vpc.application-test.id
 }
