b5447577995608f7e65f07fe5c602aec19db5774
Heath Parrott <heath.parrott@gmail.com>
2020-04-28 15:44:13 -0700

    adding outputs to core

--- modules/k8sStack/main.tf
+++ modules/k8sStack/main.tf
@@ -1,27 +1,55 @@
-#/
-Need VPC for K8S
-Need VPC for Security
+/* ## F5 Networks Secure Cloud Migration and Securty Zone Template for AWS ####################################################################################################################################################################################
+Version 1.4
+March 2020
 
 
-/#
+This Template is provided as is and without warranty or support.  It is intended for demonstration or reference purposes. While all attempts are made to ensure it functions as desired it is not a supported by F5 Networks.  This template can be used 
+to quickly deploy a Security VPC - aka DMZ, in-front of the your application VPC(S).  Additional VPCs can be added to the template by adding CIDR variables, VPC resource blocks, VPC specific route tables 
+and TransitGateway edits. Limits to VPCs that can be added are =to the limits of transit gateway.
 
-/*
+It is built to run in a region with three zones to use and will place services in 1a and 1c.  Modifications to other zones can be done.
+
+F5 Application Services will be deployed into the security VPC but if one wished they could also be deployed inside of the Application VPCs. 
+
+*/
+###############################################################################################################################################################################################################################################################
 #### Deploy Fargate Containers and Service Discovery Service ###################################################################################################################################################################################################
-#
+#  This stack is for EXAMPLE only and deploys a KNOWN VULNERABLE APPLICATION.  DO NOT USE IN ENVIRONMENTS WHERE SECURITY IS A CONCERN!!!!!!
 #  F5 can control ingress to Faregate services and leverage the DNS Name service to locate the items.  Note you will have to decrease the default time on the NODE (not pool) for DNS discovery to work efficiently.  You will need to use the DNS name 
-#  example.my-project.local for your node.
+#  juiceshop.my-project.local for your node.
+#
 #
 ################################################################################################################################################################################################################################################################
-*/
+
+
+resource "aws_security_group" "demo_tasks" {
+  name        = "demo-ecs-tasks"
+  description = "allow inbound access to Fargate Instances"
+  vpc_id      = "${module.core.container-test.id}"
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = "${var.app_port}"
+    to_port     = "${var.app_port}"
+    cidr_blocks = ["${module.core.cidr-1}", "${module.core.cidr-2}", "${module.cidr-3}"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 resource "aws_service_discovery_private_dns_namespace" "example" {
-  name        = "example.my-project.local"
+  name        = "my-project.local"
   description = "example"
-  vpc         = "${aws_vpc.security-test.id}"
+  vpc         = "${module.core.security-vpc}"
 }
 
 resource "aws_service_discovery_service" "example" {
-  name = "example"
+  name = "juiceshop"
 
   dns_config {
     namespace_id = "${aws_service_discovery_private_dns_namespace.example.id}"
@@ -78,7 +106,7 @@ resource "aws_ecs_service" "main" {
 
   network_configuration {
     security_groups = ["${aws_security_group.demo_tasks.id}"]
-    subnets         = ["${aws_subnet.app_subnet_dmz_1_region-az-1.id}", "${aws_subnet.app_subnet_dmz_1_region-az-2.id}"] #Edits required here
+    subnets         = ["${module.core.app_subnet_application_region-az-1}", "${module.core.app_subnet_application_region-az-2}"] #Edits required here
   }
 
   service_registries {
