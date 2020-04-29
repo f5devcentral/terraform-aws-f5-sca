071d10aba70b31e199a83ae7fc7d09f6042f00ae
Eric Chen <eric.chen@f5.com>
2020-04-29 15:56:56 -0400

    external selfip

--- sca/configuration_stack/externalDeclarativeOnboarding.tf
+++ sca/configuration_stack/externalDeclarativeOnboarding.tf
@@ -1,3 +1,17 @@
+locals {
+  external_bigips = flatten([
+    for environment, bigips in var.bigip_map.value : [
+      for key, bigip in bigips : {
+        id : key
+        subnets : {
+          for subnet, data in bigip : data.attachment[0].device_index => {
+            private_ip : data.private_ip
+          }
+        }
+      }
+    ] if(environment == "external")
+  ])
+}
 #retrieve secret from AWS to use in DO
 data "aws_secretsmanager_secret_version" "secret" {
   secret_id = var.secrets_manager_name.value
@@ -13,10 +27,11 @@ data "template_file" "vm01_do_json" {
     host1	        = var.ext0_host1
     host2	        = var.ext0_host2
     local_host      = var.ext0_local_host
-    local_selfip    = var.ext0_local_selfip
+#    local_selfip    = var.ext0_local_selfip
+    local_selfip    = local.external_bigips[0].subnets.0.private_ip
     local_selfip2   = var.ext0_local_selfip2
     local_selfip3   = var.ext0_local_selfip3
-    remote_host	    = local.nic_map_device_1[0].private_ip
+    remote_host	    = var.ext0_remote_host
     remote_selfip   = var.ext0_remote_selfip
     gateway	        = var.ext0_gateway
     dns_server	    = var.dns_server
