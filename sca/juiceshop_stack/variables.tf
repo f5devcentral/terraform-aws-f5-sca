6320b7ef22768f6619ce757aa0a955b9932ca33d
Mark Menger <m.menger@f5.com>
2020-04-29 18:47:59 +0000

    build juiceshop vms and setup docker

---
+++ sca/juiceshop_stack/variables.tf
@@ -0,0 +1,13 @@
+variable appnodes_per_az {
+    default = 3
+}
+variable aws_region {}
+variable project {}
+variable random_id {}
+variable secrets_manager_name {}
+variable iam_instance_profile_name {}
+variable security_groups {}
+variable vpcs {}
+variable subnets {}
+variable route_tables {}
+variable transit_gateways {}
