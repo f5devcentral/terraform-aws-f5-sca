3ce50598040317379516acfe0fe7279859b5801f
Cody Green <cody@codygreen.com>
2020-04-30 17:58:04 -0500

    BIG-IP InSpec test

--- sca/security_stack/inspec/security-stack-ready/controls/security.rb
+++ sca/security_stack/inspec/security-stack-ready/controls/security.rb
@@ -2,79 +2,10 @@
 
 title "Security Stack"
 
-# load data from Terraform output
-# created by terraform output --json > inspec/bigip-ready/files/terraform.json
-# content = inspec.profile.file("terraform.json")
-# params = JSON.parse(content)
+include_controls 'aws-instance-ready' do
 
-# begin
-#     BIGIP_MGMT_IPS    = params['bigip_mgmt_ips'][*]['value']
-#     BIGIP_MGMT_DNS    = params['bigip_mgmt_dns']['value']
-#     BIGIP_MGMT_PORT   = params['bigip_mgmt_port']['value']
-#     MGMT_ADDRESSES    = params['mgmt_addresses']['value']
-#     PUBLIC_ADDRESSES  = params['public_addresses']['value']
-#     PRIVATE_ADDRESSES = params['private_addresses']['value']
-#     PUBLIC_NIC_IDS    = params['public_nic_ids']['value']
-# rescue
-#     BIGIP_MGMT_IPS    = []
-#     BIGIP_MGMT_DNS    = []
-#     BIGIP_MGMT_PORT   = []
-#     MGMT_ADDRESSES    = []
-#     PUBLIC_ADDRESSES  = []
-#     PRIVATE_ADDRESSES = []
-#     PUBLIC_NIC_IDS    = []
-# end
+end
 
-bigip_host = input('bigip_mgmt_ip')
-bigip_mgmt_port = input('bigip_mgmt_port')
-bigip_password = input('bigip_password')
+include_controls 'bigip-atc-ready' do
 
-# 
-# Test that DO is available and the correct version
-#
-control "Declarative Onboarding Available" do
-  impact 1.0
-  title "BIGIP has DO"
-  # is the declarative onboarding end point available?
-  describe http("https://#{bigip_host}:#{bigip_mgmt_port}/mgmt/shared/declarative-onboarding/info",
-              auth: {user: 'admin', pass: bigip_password},
-              params: {format: 'html'},
-              method: 'GET',
-              ssl_verify: false) do
-          its('status') { should cmp 200 }
-          its('headers.Content-Type') { should match 'application/json' }
-  end
-  describe json(content: http("https://#{bigip_host}:#{bigip_mgmt_port}/mgmt/shared/declarative-onboarding/info",
-              auth: {user: 'admin', pass: bigip_password},
-              params: {format: 'html'},
-              method: 'GET',
-              ssl_verify: false).body) do
-          its([0,'version']) { should eq '1.12.0' }
-          its([0,'release']) { should eq '1' } # this should be replaced with a test using the json resource
-  end
-end 
-
-# 
-# Test that AS3 is available and the correct version
-#
-control "Application Service Extension Available" do
-  impact 1.0
-  title "BIGIP has AS3"
-  # is the application services end point available?
-    describe http("https://#{bigip_host}:#{bigip_mgmt_port}/mgmt/shared/appsvcs/info",
-              auth: {user: 'admin', pass: bigip_password},
-              params: {format: 'html'},
-              method: 'GET',
-              ssl_verify: false) do
-          its('status') { should cmp 200 }
-          its('headers.Content-Type') { should match 'application/json' }
-    end
-    describe json(content: http("https://#{bigip_host}:#{bigip_mgmt_port}/mgmt/shared/appsvcs/info",
-              auth: {user: 'admin', pass: bigip_password},
-              params: {format: 'html'},
-              method: 'GET',
-              ssl_verify: false).body) do
-          its('version') { should eq '3.13.2' }
-          its('release') { should eq '1' } # this should be replaced with a test using the json resource
-    end
 end
--- sca/security_stack/runtests.sh
+++ sca/security_stack/runtests.sh
@@ -4,14 +4,16 @@ tf_output_file='inspec/security-stack-ready/files/terraform.json'
 # Save the Terraform data into a JSON file for InSpec to read
 terraform output --json > $tf_output_file
 
-# TODO: uncomment once a jumphost is created
-# Set the jumphost IP address
-#jumphost=`cat $tf_output_file| jq '.jumphost_ip.value[0]' -r`
-
-# Run InSpec tests from the Jumphost
-#inspec exec inspec/security-stack-ready -t ssh://azureuser@$jumphost -i tftest
-bigips=$(jq '.mgmt_addresses.value[][]' $tf_output_file)
+# internal management interfaces - uncommented when jumphost is ready
+bigips=$(jq '.bigip_mgmt_dns.value[][]' $tf_output_file -r)
 bigip_mgmt_port=443
+bigip_instance_ids=$(terraform show --json | jq '.values.root_module.child_modules[].resources[] | select(.address=="aws_instance.f5_bigip") | .values.id' -r)
+
+for bigip_instance_id in $bigip_instance_ids
+do
+    echo $bigip_instance_id
+    inspec exec inspec/security-stack-ready --input instance_id=$bigip_instance_id -t aws:// || break
+done || exit 1
 
 # get the BIG-IP password
 secrets_manager_name=$(jq '.secrets_manager_name.value' core.auto.tfvars.json)
@@ -20,5 +22,5 @@ bigip_password=$(aws secretsmanager get-secret-value --secret-id "$secrets_manag
 for bigip in $bigips
 do
     echo $bigip
-    inspec exec inspec/security-stack-ready --input bigip_mgmt_ip=$bigip bigip_password=$bigip_password
-done
+    inspec exec inspec/security-stack-ready --input bigip_mgmt_ip=$bigip bigip_password=$bigip_password || break
+done || exit 1
