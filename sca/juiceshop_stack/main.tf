9123c51bcc2117b8bccefa4f36fae1e8b83d7adf
Mark Menger <m.menger@f5.com>
2020-04-30 01:25:31 +0000

    add commentary

--- sca/juiceshop_stack/main.tf
+++ sca/juiceshop_stack/main.tf
@@ -1,19 +1,21 @@
+# find an 18.04 ubuntu image because we want
+# to use the newer YAML cloud-init
 data "aws_ami" "ubuntu" {
   most_recent = true
-
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
   }
-
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
   owners = ["099720109477"] # Canonical
 }
 
+# create appnodes_per_az number of application servers
+# per each availability zone
+# their names in the AWS portal will look like appnode-az1-2
 resource "aws_instance" "appnode" { 
   for_each = {
     for n in range(var.appnodes_per_az * length(var.subnets.value)):
@@ -23,7 +25,6 @@ resource "aws_instance" "appnode" {
   instance_type           = "t2.micro"
   subnet_id               = each.value.application.application_region
   user_data               = file("${path.module}/cloudinit.yml")
-  #vpc_security_group_ids = [var.security_groups.value.private]
   tags = {
     Name = "appnode-${each.key}"
   }
