63421094b87fa2e579695845f8bc77e18267cf45
Mark Menger <m.menger@f5.com>
2020-04-30 02:04:53 +0000

    test for AWS state

--- sca/juiceshop_stack/inspec/application-stack-ready/controls/appstack.rb
+++ sca/juiceshop_stack/inspec/application-stack-ready/controls/appstack.rb
@@ -1,13 +1,24 @@
 # copyright: 2020, F5 Networks
 
-title "Application Stack"
+title "Application Stack AWS Configuration"
 
 # load data from Terraform output
-# created by terraform output --json > inspec/bigip-ready/files/terraform.json
+# created by terraform output --json > inspec/application-stack-ready/files/terraform.json
 content = inspec.profile.file("terraform.json")
 params = JSON.parse(content)
-
+begin
+    JUICESHOP_AWS_NAME = params['juiceshop_aws_names']['value']
+rescue 
+    JUICESHOP_AWS_NAME = []
+end
 
 #
 # TODO: what tests validate that the juiceshop stack is ready?
 #
+JUICESHOP_AWS_NAME.each do |juiceshop_node|
+    describe aws_ec2_instance(name: juiceshop_node) do
+        it { should exist }
+        it { should be_running }
+    end
+end
+
