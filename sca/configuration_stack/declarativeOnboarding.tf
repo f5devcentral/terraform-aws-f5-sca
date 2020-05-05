a0c4947d5ec36122440b2c9a84895a485939b151
Ubuntu <ubuntu@ip-10-0-0-145.ec2.internal>
2020-05-05 12:45:06 +0000

    updates to declarativeOnboarding.tf

--- sca/configuration_stack/declarativeOnboarding.tf
+++ sca/configuration_stack/declarativeOnboarding.tf
@@ -85,9 +85,8 @@ data "template_file" "ext_bigip_1_do_json" {
     local_selfip2   = local.external_bigips[1].subnets.2.private_ip
     local_selfip3   = local.external_bigips[1].subnets.3.private_ip
     remote_host	    = local.external_bigips[0].subnets.0.private_dns_name
-<<<<<<< HEAD:sca/configuration_stack/declarativeOnboarding.tf
-    remote_selfip   = local.external_bigips[0].subnets.0.private_ip
-    gateway	        = var.ext1_gateway
+    remote_selfip   = local.external_bigips[0].subnets.2.private_ip
+    gateway	        = var.ext0_gateway
     dns_server	    = var.dns_server
     ntp_server	    = var.ntp_server
     timezone	    = var.timezone
@@ -108,7 +107,7 @@ data "template_file" "ips_bigip_0_do_json" {
     local_selfip2   = local.ips_bigips[0].subnets.2.private_ip
     local_selfip3   = local.ips_bigips[0].subnets.3.private_ip
     remote_host	    = local.ips_bigips[1].subnets.0.private_dns_name
-    remote_selfip   = local.ips_bigips[1].subnets.0.private_ip
+    remote_selfip   = local.ips_bigips[1].subnets.2.private_ip
     gateway	        = var.ips0_gateway
     dns_server	    = var.dns_server
     ntp_server	    = var.ntp_server
@@ -117,6 +116,7 @@ data "template_file" "ips_bigip_0_do_json" {
     admin_password  = data.aws_secretsmanager_secret_version.secret.secret_string
   }
 }
+
 data "template_file" "ips_bigip_1_do_json" {
   template = file("${path.module}/templates/declarativeOnboarding/externalClusterPayg.json")
 
@@ -130,12 +130,53 @@ data "template_file" "ips_bigip_1_do_json" {
     local_selfip2   = local.ips_bigips[1].subnets.2.private_ip
     local_selfip3   = local.ips_bigips[1].subnets.3.private_ip
     remote_host	    = local.ips_bigips[0].subnets.0.private_dns_name
-    remote_selfip   = local.ips_bigips[0].subnets.0.private_ip
+    remote_selfip   = local.ips_bigips[0].subnets.2.private_ip
     gateway	        = var.ips1_gateway
-=======
-    remote_selfip   = local.external_bigips[0].subnets.2.private_ip
-    gateway	        = var.ext0_gateway
->>>>>>> 6bd86e7137aaf3642d58ac6a9659ab1ab1656148:sca/configuration_stack/externalDeclarativeOnboarding.tf
+    dns_server	    = var.dns_server
+    ntp_server	    = var.ntp_server
+    timezone	    = var.timezone
+    admin_user      = var.uname
+    admin_password  = data.aws_secretsmanager_secret_version.secret.secret_string
+  }
+}
+data "template_file" "internal_bigip_0_do_json" {
+  template = file("${path.module}/templates/declarativeOnboarding/externalClusterPayg.json")
+
+  vars = {
+    #Uncomment the following line for BYOL
+    #local_sku	    = "${var.license1}"
+    host1	        = local.internal_bigips[0].subnets.0.private_ip
+    host2	        = local.internal_bigips[1].subnets.0.private_ip
+    local_host      = local.internal_bigips[0].subnets.0.private_dns_name
+    local_selfip    = local.internal_bigips[0].subnets.1.private_ip
+    local_selfip2   = local.internal_bigips[0].subnets.2.private_ip
+    local_selfip3   = local.internal_bigips[0].subnets.3.private_ip
+    remote_host	    = local.internal_bigips[1].subnets.0.private_dns_name
+    remote_selfip   = local.internal_bigips[1].subnets.2.private_ip
+    gateway	        = var.int0_gateway
+    dns_server	    = var.dns_server
+    ntp_server	    = var.ntp_server
+    timezone	    = var.timezone
+    admin_user      = var.uname
+    admin_password  = data.aws_secretsmanager_secret_version.secret.secret_string
+  }
+}
+
+data "template_file" "internal_bigip_1_do_json" {
+  template = file("${path.module}/templates/declarativeOnboarding/externalClusterPayg.json")
+
+  vars = {
+    #Uncomment the following line for BYOL
+    #local_sku	    = "${var.license1}"
+    host1	        = local.internal_bigips[1].subnets.0.private_ip
+    host2	        = local.internal_bigips[0].subnets.0.private_ip
+    local_host      = local.internal_bigips[1].subnets.0.private_dns_name
+    local_selfip    = local.internal_bigips[1].subnets.1.private_ip
+    local_selfip2   = local.internal_bigips[1].subnets.2.private_ip
+    local_selfip3   = local.internal_bigips[1].subnets.3.private_ip
+    remote_host	    = local.internal_bigips[0].subnets.0.private_dns_name
+    remote_selfip   = local.internal_bigips[0].subnets.2.private_ip
+    gateway	        = var.int1_gateway
     dns_server	    = var.dns_server
     ntp_server	    = var.ntp_server
     timezone	    = var.timezone
@@ -158,6 +199,14 @@ resource "local_file" "ips_bigip_0_do_json" {
 resource "local_file" "ips_bigip_1_do_json" {
   content     = data.template_file.ips_bigip_1_do_json.rendered
   filename    = "${path.module}/ips_bigip_1_do_json.json"
+}
+resource "local_file" "internal_bigip_0_do_json" {
+  content     = data.template_file.internal_bigip_0_do_json.rendered
+  filename    = "${path.module}/internal_bigip_0_do_json.json"
+}
+resource "local_file" "internal_bigip_1_do_json" {
+  content     = data.template_file.internal_bigip_1_do_json.rendered
+  filename    = "${path.module}/internal_bigip_1_do_json.json"
 }
 
 output external_bigips {
