638efbacc5013e4f5dca984353528ede4509169c
Eric Chen <eric.chen@f5.com>
2020-05-08 16:06:37 -0400

    remove CFE

--- sca/configuration_stack/declare_cfe.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PWD=$2
-TARGET=$1
-curl -X POST -k -u ${PWD} $TARGET/mgmt/shared/cloud-failover/declare -H content-type:application/json -d @./cfe.json
-#curl -X POST -k -u ${PWD} $TARGET/mgmt/shared/cloud-failover/reset -H content-type:application/json -d '{"resetStateFile":true}'
-curl -X POST -k -u ${PWD} $TARGET/mgmt/shared/cloud-failover/trigger -H content-type:application/json -d {}
-
