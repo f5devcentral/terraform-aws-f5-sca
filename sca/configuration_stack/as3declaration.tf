ef4d514a7eb68f9ee3261f6db7f814e374979b01
Ubuntu <michael.w.oleary@gmail.com>
2020-06-27 19:44:34 +0000

    AS3 fixes for IPS tier - renamed AS3 templates to match tier names - added forwarding VS inbound to IPS tier

--- sca/configuration_stack/as3declaration.tf
+++ sca/configuration_stack/as3declaration.tf
@@ -7,7 +7,7 @@ resource "bigip_as3"  "external_bigip_az1" {
         null_resource.cfe-internal-az2
      ]
      provider = bigip.external_bigip_az1
-     as3_json = templatefile("${path.module}/templates/as3/as3.tmpl", {
+     as3_json = templatefile("${path.module}/templates/as3/external_tier.tmpl", {
       virtualAddress = "0.0.0.0/0",
       allowedVlan = "internal"
     })
@@ -19,11 +19,13 @@ resource "bigip_as3"  "external_bigip_az1" {
         bigip_do.ips_bigip_az2
      ]
      provider = bigip.ips_bigip_az1
-     as3_json = templatefile("${path.module}/templates/as3/forwarding_vs_with_pool.tmpl", {
+     as3_json = templatefile("${path.module}/templates/as3/ips_tier.tmpl", {
       virtualAddress = "0.0.0.0/0",
       allowedVlan = "internal"
       poolMember1 = local.ext_self_ip_extNic_az1[0].subnets[3].private_ip,
-      poolMember2 = local.ext_self_ip_extNic_az2[0].subnets[3].private_ip
+      poolMember2 = local.ext_self_ip_extNic_az2[0].subnets[3].private_ip,
+      virtualAddress2 = "0.0.0.0/0",
+      allowedVlan2 = "external"
     })
  }
 
@@ -33,11 +35,13 @@ resource "bigip_as3"  "external_bigip_az1" {
         bigip_do.ips_bigip_az2
      ]
      provider = bigip.ips_bigip_az2
-     as3_json = templatefile("${path.module}/templates/as3/forwarding_vs_with_pool.tmpl", {
+     as3_json = templatefile("${path.module}/templates/as3/ips_tier.tmpl", {
       virtualAddress = "0.0.0.0/0",
       allowedVlan = "internal",
       poolMember1 = local.ext_self_ip_extNic_az1[0].subnets[3].private_ip,
-      poolMember2 = local.ext_self_ip_extNic_az2[0].subnets[3].private_ip
+      poolMember2 = local.ext_self_ip_extNic_az2[0].subnets[3].private_ip,
+      virtualAddress2 = "0.0.0.0/0",
+      allowedVlan2 = "external"
     })
  }
 
@@ -49,12 +53,10 @@ resource "bigip_as3"  "external_bigip_az1" {
         null_resource.cfe-internal-az2
      ]
      provider = bigip.internal_bigip_az1
-     as3_json = templatefile("${path.module}/templates/as3/forwarding_vs_with_pool.tmpl", {
+     as3_json = templatefile("${path.module}/templates/as3/internal_tier.tmpl", {
       virtualAddress = "0.0.0.0/0",
       allowedVlan = "internal",
       poolMember1 = local.ips_self_ip_extNic_az1[0].subnets[3].private_ip,
       poolMember2 = local.ips_self_ip_extNic_az2[0].subnets[3].private_ip
     })
  }
-
-# app1 AS3 config for External and internal tiers
